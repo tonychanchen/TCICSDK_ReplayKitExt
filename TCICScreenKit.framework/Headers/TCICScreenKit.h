c4e6a691e7303956d509a743ac9d3b5d4b31fb79
alexichen <alexichen@tencent.com>
2021-08-26 21:25:13 +0800

    更新版本: 1.0.0.29

--- TCICScreenKit.framework/Headers/TCICScreenKit.h
+++ TCICScreenKit.framework/Headers/TCICScreenKit.h
@@ -5,8 +5,8 @@
 //  Created by 陈耀武 on 2021/1/13.
 //  Copyright © 2021 陈耀武. All rights reserved.
 //
-// TCICScreenKitVersion : 1.0.0.2281
-// TCICScreenKitGitCommitID : 8e3fbc764f56e9516a64e7f8289aeb45af1eb9d4
+// TCICScreenKitVersion : 1.0.0.2451
+// TCICScreenKitGitCommitID : a25b549a53b88ec060e38740ef6e9e2287a2cc8b
 
 #import <Foundation/Foundation.h>
 #import <ReplayKit/ReplayKit.h>
@@ -40,7 +40,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
 * 在- (void)processSampleBuffer中调用，通知TCICSDK处理视频帧的设备方向
 */
-- (void)processSampleBuffer:(CMSampleBufferRef)sampleBuffer withType:(RPSampleBufferType)sampleBufferType API_AVAILABLE(ios(11.1));
+- (void)processSampleBuffer:(CMSampleBufferRef)sampleBuffer withType:(RPSampleBufferType)sampleBufferType;
 @end
 
 NS_ASSUME_NONNULL_END
